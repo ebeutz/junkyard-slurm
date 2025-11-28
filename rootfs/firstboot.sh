669156485bbcd81b91fc5b8fccbfc5c127399afe
ebeutz <ebeutz@ucsd.edu>
2025-11-28 08:58:11 +0000

    add adbd

--- rootfs/firstboot.sh
+++ rootfs/firstboot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # This file would be executed on first boot
 echo TODO: write first boot script
+sudo systemctl enable adbd.service
+sudo systemctl start adbd.service
 sed -i '/firstboot.sh/d' ~/.profile
